b797878a319b435d11057b2d886039a359de866d
Alexandra Beida <aleksandra.bejda@privatbank.ua>
2021-11-02 20:44:53 +0200

    init commit

--- src/com/pb/beida/hw2/Interval.java
+++ src/com/pb/beida/hw2/Interval.java
@@ -1,28 +1,27 @@
 package com.pb.beida.hw2;
 
-import java.util.Scanner;
-
 public class Interval {
     public static void main(String[] args) {
         int a = 50;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("input x: ");
-        int x = scanner.nextInt();
-        System.out.println("input y: ");
-        int y = scanner.nextInt();
-
-        if (a > 0 && a < 14) {
-           System.out.println("подходящий промежуток [0-14]");
+        if (a >= 0 && a <= 14) {
+           System.out.print("Это подходящий промежуток [0-14]");
+        } else {
+            System.out.print("Промежуток [0-14] не подходит");
         }
-        if (a > 15 && a < 35){
-            System.out.println("подходящий промежуток [15-35]");
+        if (a >= 15 && a <= 35){
+            System.out.print("Это подходящий промежуток [15-35]");
+        } else {
+            System.out.print("Промежуток [15-35] не подходит");
         }
-        if (a > 36 && a < 50){
-            System.out.println("подходящий промежуток [36-50]");
+        if (a >= 36 && a <= 50){
+            System.out.println("Это подходящий промежуток [36-50]");
+        } else {
+            System.out.print("Промежуток [36-50] не подходит");
         }
-        if (a > 51 && a < 100){
-            System.out.println("подходящий промежуток [51-100]");
+        if (a >= 51 && a <= 100){
+            System.out.println("Это подходящий промежуток [51-100]");
+        } else {
+            System.out.print("Промежуток [51-100] не подходит");
         }
     }
 }
